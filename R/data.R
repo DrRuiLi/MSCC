043edb342ad9c5ad578356a4f4d9926612171ae4
LiRui <918791461@qq.com>
2026-08-08 17:38:49 +0800

    feat: add demo chemical formulas and SMILES strings datasets
    - Introduced `demo_chemform` and `demo_smi` datasets for use in examples and benchmarking of chemform helpers.
    - Removed obsolete `chem_formula_template.rda` file to streamline data assets.
    - Updated documentation to provide details on the new datasets and their formats.

---
+++ R/data.R
@@ -0,0 +1,22 @@
+#' Demo chemical formulas
+#'
+#' Character vector of example chemical formulas for demos, examples, and
+#' benchmarking of chemform helpers.
+#'
+#' @format A character vector of length 107.
+#' @docType data
+#' @name demo_chemform
+#' @examples
+#' head(demo_chemform)
+"demo_chemform"
+
+#' Demo SMILES strings
+#'
+#' Character vector of example SMILES for demos and examples.
+#'
+#' @format A character vector of length 100.
+#' @docType data
+#' @name demo_smi
+#' @examples
+#' head(demo_smi)
+"demo_smi"
